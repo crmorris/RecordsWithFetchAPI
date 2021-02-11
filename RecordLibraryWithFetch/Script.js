383ffee6ac869789648579fda081fbbadce345a9
crmorris <33641568+crmorris@users.noreply.github.com>
2021-02-11 17:09:40 -0600

    Pinned Purchase Button

--- RecordLibraryWithFetch/Script.js
+++ RecordLibraryWithFetch/Script.js
@@ -12,10 +12,10 @@
             output +=
             `<div class="card col-md-3 m-3" style="width: 18rem;">
                 <img class="card-img-top" src="./Record.jpg" alt="Card image cap">
-                <div class="card-body">
+                <div class="card-body d-flex flex-column">
                     <h5 class="card-title">${user.title.replace(/(^\w{1})|(\s+\w{1})/g, letter => letter.toUpperCase())}</h5>
                     <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                    <a href="#" class="btn btn-primary">Purchase</a>
+                    <a href="#" class="btn btn-primary mt-auto">Purchase</a>
                 </div>
             </div>`;
 
